9b9232f22b468cc29f2575a78be8e1ad91e73aa5
Sergio <16337833+Sergi0Martin@users.noreply.github.com>
2024-01-02 12:45:06 +0100

    wip

--- keyboards/crkbd/keymaps/Sergi0Martin/config.h
+++ keyboards/crkbd/keymaps/Sergi0Martin/config.h
@@ -1,16 +1,8 @@
 #pragma once
 
-//#define USE_MATRIX_I2C
-
-/* Select hand configuration */
-
 #define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
-
-//#define QUICK_TAP_TERM 0
-//#define TAPPING_TERM 100
-
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_WPM_ENABLE //Enable WPM across split keyboards (+268).
 
 // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_oled_driver.md
 #ifdef OLED_ENABLE
@@ -18,15 +10,10 @@
     // #define OLED_DISPLAY_64X128
 #endif
 
-
 #ifdef RGB_MATRIX_ENABLE
     #define RGB_MATRIX_TIMEOUT 60000 // number of milliseconds to wait until rgb automatically turns off
     #define RGB_MATRIX_KEYPRESSES // reacts to keypresses
-    // #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
     #define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-    // #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-    // #   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases   keyboard responsiveness)
-    // #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to  60fps (increases keyboard responsiveness)
     #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash. 
     #define RGB_MATRIX_HUE_STEP 8
     #define RGB_MATRIX_SAT_STEP 8
@@ -39,9 +26,9 @@
     // For full list of effects, see:
     // https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
     #define ENABLE_RGB_MATRIX_ALPHAS_MODS
-    #define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-    #define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
-    #define ENABLE_RGB_MATRIX_BREATHING
+    // #define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+    // #define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+    // #define ENABLE_RGB_MATRIX_BREATHING
     // #define ENABLE_RGB_MATRIX_BAND_SAT
     // #define ENABLE_RGB_MATRIX_BAND_VAL
     // #define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
@@ -55,7 +42,7 @@
     // #define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
     // #define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
     // #define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
-    #define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+    // #define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
     // #define ENABLE_RGB_MATRIX_DUAL_BEACON
     // #define ENABLE_RGB_MATRIX_RAINBOW_BEACON
     // #define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
@@ -81,6 +68,6 @@
     // #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
     // #define ENABLE_RGB_MATRIX_SPLASH
     // #define ENABLE_RGB_MATRIX_MULTISPLASH
-    #define ENABLE_RGB_MATRIX_SOLID_SPLASH
-    #define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+    // #define ENABLE_RGB_MATRIX_SOLID_SPLASH
+    // #define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
 #endif
